491dfc7723f578f3a0cd8a439329ab692ddb8e9f
Han <chelllagom@gmail.com>
2021-02-22 01:40:06 +0900

    Revert "Revert "객체, 비구조 할당 학습""
    This reverts commit 0bb4d85cf3bdcf9955c2984059914c94ded4d1f3.

--- object.js
+++ object.js
@@ -1,17 +1,30 @@
 //객체
 const dog = {
-    key : 'value',
     name : '멍멍이',
     age : 2,
-    sample : {
-        a : 1,
-        b : 2,
+    sound: 'mung mung!',
+    say(){
+        console.log(this.sound);
     }
-}
+};
+
 
 console.log(dog.name);
 console.log(dog);
 
+//객체 추가 생성
+const cat = {
+    name : '야옹이',
+    sound : '야옹!',
+};
+
+cat.say = dog.say;
+dog.say();
+cat.say();
+
+//function을 꺼내 사용하면 this가 값을 찾지 못한다!
+const catSay = cat.say;
+catSay();
 
 //비구조 할당-객체 구조 분해 : 객체 내부의 key 값을 사용할 수 있게 해줌
 const { age } = dog;
