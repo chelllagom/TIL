5abe98439292262719e6c69eca2b3dc296961548
Han <chelllagom@gmail.com>
2023-01-20 16:00:14 +0900

    ing

--- story-game-prj/app.js
+++ story-game-prj/app.js
@@ -1,7 +1,55 @@
-import {gc} from "./panel/game-canvas.js"
-import {sc} from "./panel/story-canvas.js";
+import StoryCanvas from "./panel/story-canvas.js";
+import WindGameCanvas from "./panel/wind-game-canvas.js";
+import {gc} from "./panel/fire-game-canvas.js";
 
 window.addEventListener("load",function(){
-       gc.run();
-       sc.run();
+       const storyCanvas = new StoryCanvas();
+       const pigwindgameCanvas = document.querySelector(".wind-game-canvas");
+       const pighomegameCanvas = document.querySelector(".pighomegame-canvas");
+       let pig1homegame;
+       let pig3homegame;
+
+       storyCanvas.checkCurPage = (page) => {
+              if(page === 14){                               
+                     storyCanvas.dom.classList.add("d-none");
+                     pigwindgameCanvas.classList.remove("d-none");                     
+                     pig1homegame = new WindGameCanvas(1);
+                     pig1homegame.onnextGame=()=>{
+                            storyCanvas.dom.classList.remove("d-none");
+                            pigwindgameCanvas.classList.add("d-none");
+                            storyCanvas.page = 14;
+                            pig1homegame.pause = true;
+                            // storyCanvas.run();
+                     }
+                     pig1homegame.run();
+              }             
+              else if(page === 17){
+                     storyCanvas.dom.classList.add("d-none");
+                     pigwindgameCanvas.classList.remove("d-none");                     
+                     pig3homegame = new WindGameCanvas(3);
+                     pig3homegame.onnextGame=() => {
+                            storyCanvas.dom.classList.remove("d-none");
+                            pigwindgameCanvas.classList.add("d-none");
+                            storyCanvas.page = 17;
+                            pig3homegame.pause = true;
+                            // storyCanvas.run();
+                     }
+                     pig3homegame.run();
+              }
+              else if(page === 20){
+                     storyCanvas.dom.classList.add("d-none");
+                     gc.dom.classList.remove("d-none");
+                     gc.gameOver = ()=>{
+                            storyCanvas.dom.classList.remove("d-none");
+                     }
+                     gc.run();
+              }              
+              else{
+                     storyCanvas.dom.classList.add("d-none");              
+                     pighomegameCanvas.classList.remove("d-none");                       
+              }
+              
+       }     
+       
+       storyCanvas.run();
 });
